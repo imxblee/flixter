0ab791fbef1d4b0aa370f6a4e0f809b310b75018
imxblee <transcendencez@gmail.com>
2016-01-26 12:58:37 +0000

    lesson 17

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,17 @@ Flixter::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'static_pages#index'
-
-resources :courses,only:[:index,:show]
-namespace :instructor do
+  root 'static_pages#index'
+  resources :courses, only: [:index, :show]
+  namespace :instructor do
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
   end
+end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
